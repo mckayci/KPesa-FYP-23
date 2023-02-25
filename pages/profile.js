fcdbc4f29abaf85a00c02499b13f6892e693a656
Ciaran McKay <82404062+mckayci@users.noreply.github.com>
2023-02-25 18:50:57 +0000

    changes apostrophe in profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -377,7 +377,7 @@ export default function Example() {
                           </option>
                           <option value="Cook Islands">Cook Islands</option>
                           <option value="Costa Rica">Costa Rica</option>
-                          <option value="Cote D'ivoire">Cote D'ivoire</option>
+                          <option value="Cote D'ivoire">Cote D&apos;ivoire</option>
                           <option value="Croatia">Croatia</option>
                           <option value="Cuba">Cuba</option>
                           <option value="Cyprus">Cyprus</option>
@@ -456,7 +456,7 @@ export default function Example() {
                           <option value="Kenya">Kenya</option>
                           <option value="Kiribati">Kiribati</option>
                           <option value="Korea, Democratic People's Republic of">
-                            Korea, Democratic People's Republic of
+                            Korea, Democratic People&apos;s Republic of
                           </option>
                           <option value="Korea, Republic of">
                             Korea, Republic of
@@ -464,7 +464,7 @@ export default function Example() {
                           <option value="Kuwait">Kuwait</option>
                           <option value="Kyrgyzstan">Kyrgyzstan</option>
                           <option value="Lao People's Democratic Republic">
-                            Lao People' s Democratic Republic
+                            Lao People&apos;s Democratic Republic
                           </option>
                           <option value="Latvia">Latvia</option>
                           <option value="Lebanon">Lebanon</option>

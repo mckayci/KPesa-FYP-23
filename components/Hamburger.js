85d90bf5b024f3acb40099a1746af8d027cb0f4b
Ciaran McKay <82404062+mckayci@users.noreply.github.com>
2023-02-25 21:31:11 +0000

    changed image/next

--- components/Hamburger.js
+++ components/Hamburger.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../public/images/KPesa_logo-300x300.png";
 import { CgProfile } from "react-icons/cg";
 import { useAuth } from "../hooks/useAuth";
 import { userAgent } from "next/server";

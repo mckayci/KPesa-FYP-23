22c251c420d4a7f35cc77b4b6c5c1ca5ee8f5f6c
Ciaran McKay <82404062+mckayci@users.noreply.github.com>
2023-03-05 19:03:55 +0000

    fix on click handler

--- components/investment/Campaign[id].js
+++ components/investment/Campaign[id].js
@@ -118,7 +118,7 @@ function UserCampaign({ handleClick }) {
         <button
           type="button"
           className="w-full text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-1"
-          onClick={handleChange()}
+          onClick={handleChange}
         >
           Invest Today
         </button>

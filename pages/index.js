74324273bb69c52a77c2951708da691a8b2cdbf9
Ciaran McKay <82404062+mckayci@users.noreply.github.com>
2023-03-28 11:47:49 +0100

    Fixed cases for login

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import "firebase/auth";
 import "firebase/firestore";
 import { useAuth } from "../hooks/useAuth";
 import ProtectedRoute from "../components/ProtectedRoute";
-import Login from "../components/login/login";
+import Login from "../components/login/Login";
 
 export default function HomePage() {
   const { user, signInWithGoogle, signOut, db } = useAuth();
